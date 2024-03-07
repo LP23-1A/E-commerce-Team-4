f6e40f73306162b2612e036456c7af132f6f3432
Chinguun Sainbat <23LP1573@G303-CC0163.local>
2024-03-07 13:37:20 +0800

    Product table dussan

--- frontend/src/app/Product/page.tsx
+++ frontend/src/app/Product/page.tsx
@@ -101,6 +101,7 @@ export default function Product() {
                       <path d="M15.755 14.255H14.965L14.685 13.985C15.665 12.845 16.255 11.365 16.255 9.755C16.255 6.165 13.345 3.255 9.755 3.255C6.165 3.255 3.255 6.165 3.255 9.755C3.255 13.345 6.165 16.255 9.755 16.255C11.365 16.255 12.845 15.665 13.985 14.685L14.255 14.965V15.755L19.255 20.745L20.745 19.255L15.755 14.255ZM9.755 14.255C7.26501 14.255 5.255 12.245 5.255 9.755C5.255 7.26501 7.26501 5.255 9.755 5.255C12.245 5.255 14.255 7.26501 14.255 9.755C14.255 12.245 12.245 14.255 9.755 14.255Z" fill="#121316"/>
                     </svg>
                   </div>
+                  <p></p>
                   <input className="appearance-none border-2 pl-10 border-gray-300 hover:border-gray-400 transition-colors rounded-md w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:ring-purple-600 focus:border-purple-600 focus:shadow-outline"
                     type="text"
                     placeholder="Бүтээгдэхүүний нэр, SKU, UPC"
