a920a65aadf7b1f4c7564cfd2520e8f8c7d61032
батчулуун цогбаяр <23LP2014@G303-CC0138.local>
2024-03-12 13:01:47 +0800

    order frontend hiisen

--- frontend/src/app/order/page.tsx
+++ frontend/src/app/order/page.tsx
@@ -12,18 +12,21 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const API = "http://localhost:8000/order";
 const page = () => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [orderData, setOrderData] = useState([]);
+  const [activeButton, setActiveButton] = useState(1);
   const { data, error, isLoading } = useSWR(API, fetcher);
-  console.log(data);
 
   const handleStatus = (index: number) => {
     setActiveIndex(index);
+  };
+  let a: any = [];
+  const handleButton = (index: number) => {
+    setActiveButton(index);
   };
 
   return (
     <div>
       <Navbar />
-      <div className="flex">
+      <div className="flex w-[1440px] m-auto">
         <AsideBar />
         <div className="bg-gray-200 w-full ">
           <div className="flex ">
@@ -42,14 +45,39 @@ const page = () => {
               );
             })}
           </div>
-          <div className="p-8 flex flex-col gap-5 font-bold">
+          <div className="p-8  flex flex-col gap-5 font-bold">
             <div className="flex justify-between">
               <div className="flex gap-3">
-                <button className="p-2 bg-green-600 text-white rounded">
+                <button
+                  className="p-2 bg-white rounded"
+                  style={{
+                    backgroundColor: activeButton === 1 ? "green" : "",
+                    color: activeButton === 1 ? "white" : "",
+                  }}
+                  value={0}
+                  onClick={() => handleButton(1)}
+                >
                   Өнөөдөр
                 </button>
-                <button className="p-2 bg-white rounded">7 Хоног</button>
-                <button className="flex p-2 bg-white justify-center items-center rounded gap-2">
+                <button
+                  className="p-2 bg-white rounded"
+                  value={1}
+                  style={{
+                    backgroundColor: activeButton === 7 ? "green" : "",
+                    color: activeButton === 7 ? "white" : "",
+                  }}
+                  onClick={() => handleButton(7)}
+                >
+                  7 Хоног
+                </button>
+                <button
+                  style={{
+                    backgroundColor: activeButton === 30 ? "green" : "",
+                    color: activeButton === 30 ? "white" : "",
+                  }}
+                  onClick={() => handleButton(30)}
+                  className="flex p-2 bg-white justify-center items-center rounded gap-2"
+                >
                   <Calendar />
                   <span>Сарааар</span>
                   <ExpandMore />
@@ -60,32 +88,36 @@ const page = () => {
                 <input type="text" placeholder="Дугаар, Имэйл" />
               </div>
             </div>
-            <div className="bg-white h-fit ">
+            <div className="bg-white h-fit border rounded-xl border-gray-300 w-[1100px]">
               <h1 className="text-[36px] p-4">Захиалга</h1>
-              <div className="bg-gray-200 py-6 w-full">
+              <div className="bg-gray-200 py-6 w-full flex flex-col gap-4">
                 <table>
                   <tr>
                     <th className="w-[200px]">Захиалгын ID дугаар</th>
-                    <th className="w-[160px]">Захиалагч</th>
-                    <th className="w-[200px]">Огноо</th>
+                    <th className="w-[160px] pl-3">Захиалагч</th>
+                    <th className="w-[200px] pr-6">Огноо</th>
                     <th className="w-[100px]">Цаг</th>
-                    <th className="w-[100px]">Төлбөр</th>
-                    <th className="w-[200px]">Статус</th>
-                    <th className="w-[100px]">Дэлгэрэнгүй</th>
+                    <th className="w-[100px] pl-4">Төлбөр</th>
+                    <th className="w-[200px] pr-2">Статус</th>
+                    <th className="pr-6">Дэлгэрэнгүй</th>
                   </tr>
                 </table>
-                {data?.getAllOrder.map((el: any) => {
-                  return (
-                    <UserOrder
-                      key={el._id}
-                      orderNumber={el.orderNumber}
-                      date={el.createdAt.slice(0, 10)}
-                      time={el.createdAt.slice(11, 16)}
-                      status={el.status}
-                      price={el.amountPaid}
-                    />
-                  );
-                })}
+              </div>
+              <div className="bg-white rounded-xl px-4 flex flex-col ">
+                {data?.getAllOrder
+                  .filter((e: any) => e.createdAt.slice(9, 10) >= activeButton)
+                  .map((el: any) => {
+                    return (
+                      <UserOrder
+                        key={el._id}
+                        orderNumber={el.orderNumber}
+                        date={el.createdAt.slice(0, 10)}
+                        time={el.createdAt.slice(11, 16)}
+                        status={el.status}
+                        price={el.amountPaid}
+                      />
+                    );
+                  })}
               </div>
             </div>
           </div>
--- frontend/src/components/UserOrder.tsx
+++ frontend/src/components/UserOrder.tsx
@@ -1,4 +1,7 @@
 import Arrowlogo from "@/images/Arrowlogo";
+import ExpandMore from "@/images/ExpandMore";
+import Right from "@/images/Right";
+import { useRouter } from "next/navigation";
 import React from "react";
 type props = {
   orderNumber: String;
@@ -14,25 +17,34 @@ const UserOrder: React.FC<props> = ({
   price,
   status,
 }) => {
+  const router = useRouter();
+  const handler = () => {
+    router.push("/orderDetail");
+  };
   return (
-    <div>
-      <tr>
-        <td className="w-[200px] text-[14px]">{orderNumber}</td>
-        <td className="w-[250px] text-[14px]">
+    <div className="py-2">
+      <tr className="flex items-center text-black">
+        <td className="w-[200px] text-[14px] pl-2">#{orderNumber}</td>
+        <td className="w-[250px] text-[14px] pl-24">
           <p>ZolooSoko</p>
-          <p>ZolooSoko@gmail.com</p>
+          <p className="text-[14px] font-extralight">ZolooSoko@gmail.com</p>
         </td>
-        <td className="w-[100px] text-[14px]">{date}</td>
-        <td className="w-[100px] text-[14px]">{time}</td>
-        <td className="w-[100px] text-[14px]">{price}</td>
-        <td className="w-[200px] text-[14px] ">
-          <p>{status}</p>
+        <td className="w-[200px] text-[14px] pl-10 font-light">{date}</td>
+        <td className="w-[100px] text-[14px] pl-20 font-light">{time}</td>
+        <td className="w-[100px] text-[14px] pl-20 font-light">{price}</td>
+        <td className="w-[200px] text-[14px] pl-24">
+          <p className="px-10 py-2 flex justify-center bg-green-300 rounded-xl font-light">
+            {status}
+          </p>
         </td>
-        <td className="w-[200px] text-[14px]">
-          <Arrowlogo />
+        <td
+          className="w-[200px] text-[14px] pl-24 cursor-pointer"
+          onClick={handler}
+        >
+          <Right />
         </td>
       </tr>
-      <div className="border-b border-black"></div>
+      <div className="border-b border-gray-200 mt-3"></div>
     </div>
   );
 };
