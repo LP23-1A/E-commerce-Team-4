2586d46a7c25ae6bf7fb8c6a877633d1cdd7bfc2
khangerel14 <162080218+khangerel14@users.noreply.github.com>
2024-03-14 13:50:50 +0800

    fix comflict (#59)
    Co-authored-by: Khangerel Be-ochir <23LP5567@G303-CC0142.local>

--- backend/src/controller/product.ts
+++ backend/src/controller/product.ts
@@ -1,6 +1,31 @@
 import { Request, Response } from "express";
 import { ProductModel } from "../model/product";
 
+
+// const AWS = require('aws-sdk');
+// const fs = require('fs');
+// const s3 = new AWS.S3();
+
+// AWS.config.update({
+//     region: 'ap-southeast-1',
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+// });
+
+// // const params = {
+// //     Bucket: 'ecommerceteam4pinecone',
+// //     Key: 'Character.svg',
+// //     Body: fs.createReadStream('/Users/23LP5567/documents/E-commerce-team-4/frontend/public/Character.svg')
+// // };
+
+// s3.upload(params, (err: Error, data: String) => {
+//     if (err) {
+//         console.log('Error uploading file:', err);
+//     } else {
+//         console.log('File uploaded successfully.');
+//     }
+// });
+
 type ProductType = {
   productName: String;
   categoryId: String;
--- frontend/src/app/addProduct/page.tsx
+++ frontend/src/app/addProduct/page.tsx
@@ -23,7 +23,6 @@ const page = () => {
   });
   const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
-  const openModal = () => {};
   const createProduct = async () => {
     try {
       const res = await axios.post("http://localhost:8000/products/product", {
@@ -66,152 +65,55 @@ const page = () => {
                       <div className="w-[540px] bg-white rounded-xl p-6 gap-2 flex flex-col">
                         <div className="flex flex-col gap-1">
                           <h1 className="font-semibold">Бүтээгдэхүүн нэр</h1>
-                          <input
-                            type="text"
-                            className="border w-full p-3 rounded-lg"
-                            placeholder="Нэр"
-                            onChange={(e) =>
-                              setInput((prev) => ({
-                                ...prev,
-                                productName: e.target.value,
-                              }))
-                            }
-                          />
+                          <input type="text" className="border w-full p-3 rounded-lg" placeholder="Нэр" onChange={(e) => setInput((prev) => ({ ...prev, productName: e.target.value }))}/>
                         </div>
                         <div className="flex flex-col gap-1">
                           <h1 className="font-semibold">Нэмэлт мэдээлэл</h1>
-                          <input
-                            type="text"
-                            className="border w-full p-3 rounded-lg h-fit"
-                            placeholder="Гол онцлог, давуу тал, техникийн үзүүлэлтүүдийг онцолсон дэлгэрэнгүй, сонирхолтой тайлбар"
-                            onChange={(e) =>
-                              setInput((prev) => ({
-                                ...prev,
-                                description: e.target.value,
-                              }))
-                            }
-                          />
+                          <input type="text" className="border w-full p-3 rounded-lg h-fit" placeholder="Гол онцлог, давуу тал, техникийн үзүүлэлтүүдийг онцолсон дэлгэрэнгүй, сонирхолтой тайлбар" onChange={(e) => setInput((prev) => ({ ...prev, description: e.target.value }))}/>
                         </div>
                         <div className="flex flex-col gap-1">
                           <h1 className="font-semibold">Барааны код</h1>
-                          <input
-                            type="text"
-                            className="border w-full p-3 rounded-lg"
-                            placeholder="#12345678"
-                            onChange={(e) =>
-                              setInput((prev) => ({
-                                ...prev,
-                                qty: e.target.value,
-                              }))
-                            }
-                          />
+                          <input type="text" className="border w-full p-3 rounded-lg" placeholder="#12345678" onChange={(e) => setInput((prev) => ({ ...prev, qty: e.target.value }))}/>
                         </div>
                       </div>
                       <div className="w-[540px] h-52 bg-white rounded-xl p-6 gap-2 flex flex-col">
-                        <h1 className="font-semibold text-xl">
-                          Бүтээгдэхүүн зураг
-                        </h1>
+                        <h1 className="font-semibold text-xl">Бүтээгдэхүүн зураг</h1>
                         <div className="flex gap-6 justify-start items-center h-full">
                           <div className="flex items-center justify-center">
-                            <input
-                              type="image"
-                              src=""
-                              alt=""
-                              className="flex justify-center items-center h-[124px] w-[124px] border border-dashed rounded-xl relative"
-                            />
-                            <div className="absolute">
-                              <Img />
-                            </div>
+                            <input type="image" src="" alt="" className="flex justify-center items-center h-[124px] w-[124px] border border-dashed rounded-xl relative"/>
+                            <div className="absolute"><Img /></div>
                           </div>
                           <div className="flex items-center justify-center">
-                            <input
-                              type="image"
-                              src=""
-                              alt=""
-                              className="flex justify-center items-center h-[124px] w-[124px] border border-dashed rounded-xl relative"
-                            />
-                            <div className="absolute">
-                              <Img />
-                            </div>
+                            <input type="image" src="" alt="" className="flex justify-center items-center h-[124px] w-[124px] border border-dashed rounded-xl relative"/>
+                            <div className="absolute"><Img /></div>
                           </div>
                           <div className="flex items-center justify-center">
-                            <input
-                              type="image"
-                              src=""
-                              alt=""
-                              className="flex justify-center items-center h-[124px] w-[124px] border border-dashed rounded-xl relative"
-                            />
-                            <div className="absolute">
-                              <Img />
-                            </div>
+                            <input type="image" src="" alt="" className="flex justify-center items-center h-[124px] w-[124px] border border-dashed rounded-xl relative"/>
+                            <div className="absolute"><Img /></div>
                           </div>
-                          <button className="h-10 w-10 rounded-[50%] bg-gray-200 flex justify-center items-center left-3">
-                            <Plus />
-                          </button>
+                          <button className="h-10 w-10 rounded-[50%] bg-gray-200 flex justify-center items-center left-3"><Plus /></button>
                         </div>
                       </div>
                       <div className="w-[540px] h-[163px] bg-white rounded-xl p-6 gap-2 flex justify-between">
                         <div className="flex flex-col gap-1">
                           <h1 className="font-semibold">Үндсэн үнэ</h1>
-                          <input
-                            type="text"
-                            placeholder="Үндсэн үнэ"
-                            className="border p-2 rounded-lg w-[230px]"
-                            onChange={(e) =>
-                              setInput((prev) => ({
-                                ...prev,
-                                price: e.target.value,
-                              }))
-                            }
-                          />
+                          <input type="text" placeholder="Үндсэн үнэ" className="border p-2 rounded-lg w-[230px]" onChange={(e) => setInput((prev) => ({ ...prev, price: e.target.value }))}/>
                         </div>
                         <div className="flex flex-col gap-1">
                           <h1 className="font-semibold">Үлдэгдэл тоо ширхэг</h1>
-                          <input
-                            type="text"
-                            placeholder="Үлдэгдэл тоо ширхэг"
-                            className="border p-2 rounded-lg w-[230px]"
-                            onChange={(e) =>
-                              setInput((prev) => ({
-                                ...prev,
-                                residual: e.target.value,
-                              }))
-                            }
-                          />
+                          <input type="text" placeholder="Үлдэгдэл тоо ширхэг" className="border p-2 rounded-lg w-[230px]" onChange={(e) => setInput((prev) => ({ ...prev, residual: e.target.value }))}/>
                         </div>
                       </div>
                     </div>
                     <div className="flex flex-col gap-5">
                       <div className="w-[540px] bg-white rounded-xl p-6 gap-2 flex flex-col">
                         <div className="flex flex-col gap-1">
-                          <h1 className="font-semibold text-lg">
-                            Ерөнхий ангилал
-                          </h1>
-                          <input
-                            type="text"
-                            className="border w-full p-3 rounded-lg h-16"
-                            placeholder="Сонгох"
-                            onChange={(e) =>
-                              setInput((prev) => ({
-                                ...prev,
-                                mainCate: e.target.value,
-                              }))
-                            }
-                          />
+                          <h1 className="font-semibold text-lg">Ерөнхий ангилал</h1>
+                          <input type="text" className="border w-full p-3 rounded-lg h-16" placeholder="Сонгох" onChange={(e) => setInput((prev) => ({ ...prev, mainCate: e.target.value }))}/>
                         </div>
                         <div className="flex flex-col gap-1">
                           <h1 className="font-semibold text-lg">Дэд ангилал</h1>
-                          <input
-                            type="text"
-                            className="border w-full p-3 rounded-lg h-16"
-                            placeholder="Сонгох"
-                            onChange={(e) =>
-                              setInput((prev) => ({
-                                ...prev,
-                                subCate: e.target.value,
-                              }))
-                            }
-                          />
+                          <input type="text" className="border w-full p-3 rounded-lg h-16" placeholder="Сонгох" onChange={(e) => setInput((prev) => ({ ...prev, subCate: e.target.value }))}/>
                         </div>
                       </div>
                       <div className="w-[540px] bg-white rounded-xl p-6 gap-4 flex flex-col">
@@ -221,51 +123,23 @@ const page = () => {
                           <div className="h-10 w-10 rounded-[50%] bg-black"></div>
                           <div className="h-10 w-10 rounded-[50%] bg-red-700"></div>
                           <div className="h-10 w-10 rounded-[50%] bg-purple-700"></div>
-                          <button className="h-10 w-10 rounded-[50%] bg-gray-200 flex justify-center items-center">
-                            <Plus />
-                          </button>
+                          <button className="h-10 w-10 rounded-[50%] bg-gray-200 flex justify-center items-center"><Plus /></button>
                         </div>
                         <div className="flex gap-7 items-center">
                           <p>Хэмжээ</p>
-                          <div className="h-10 w-10 rounded-[50%] bg-gray-200 flex items-center justify-center">
-                            M
-                          </div>
-                          <div className="h-10 w-10 rounded-[50%] bg-gray-200 flex items-center justify-center">
-                            L
-                          </div>
-                          <div className="h-10 w-10 rounded-[50%] bg-gray-200 flex items-center justify-center">
-                            XL
-                          </div>
-                          <div className="h-10 w-10 rounded-[50%] bg-gray-200 flex items-center justify-center">
-                            2XL
-                          </div>
-                          <div className="h-10 w-10 rounded-[50%] bg-gray-200 flex items-center justify-center">
-                            3XL
-                          </div>
-                          <button className="h-10 w-10 rounded-[50%] bg-gray-200 flex justify-center items-center">
-                            <Plus />
-                          </button>
+                          <div className="h-10 w-10 rounded-[50%] bg-gray-200 flex items-center justify-center">M</div>
+                          <div className="h-10 w-10 rounded-[50%] bg-gray-200 flex items-center justify-center">L</div>
+                          <div className="h-10 w-10 rounded-[50%] bg-gray-200 flex items-center justify-center">XL</div>
+                          <div className="h-10 w-10 rounded-[50%] bg-gray-200 flex items-center justify-center">2XL</div>
+                          <div className="h-10 w-10 rounded-[50%] bg-gray-200 flex items-center justify-center">3XL</div>
+                          <button className="h-10 w-10 rounded-[50%] bg-gray-200 flex justify-center items-center"><Plus /></button>
                         </div>
-                        <button className="border p-3 px-5 w-fit rounded-md font-semibold">
-                          Төрөл нэмэх
-                        </button>
+                        <button className="border p-3 px-5 w-fit rounded-md font-semibold">Төрөл нэмэх</button>
                       </div>
                       <div className="w-[540px] bg-white rounded-xl p-6 gap-2 flex flex-col">
                         <h1 className="font-semibold text-lg">Таг</h1>
-                        <input
-                          type="text"
-                          className="border w-full p-3 rounded-lg h-16"
-                          placeholder="Таг нэмэх..."
-                          onChange={(e) =>
-                            setInput((prev) => ({
-                              ...prev,
-                              tag: e.target.value,
-                            }))
-                          }
-                        />
-                        <p className="text-gray-600">
-                          Санал болгох: Гутал, Цүнх, Эмэгтэй
-                        </p>
+                        <input type="text" className="border w-full p-3 rounded-lg h-16" placeholder="Таг нэмэх..." onChange={(e) => setInput((prev) => ({ ...prev, tag: e.target.value,}))}/>
+                        <p className="text-gray-600">Санал болгох: Гутал, Цүнх, Эмэгтэй</p>
                       </div>
                     </div>
                   </div>
@@ -273,12 +147,7 @@ const page = () => {
                     <button className="border p-3 px-5 text-black bg-white rounded-lg">
                       Ноорог
                     </button>
-                    <button
-                      className="border p-3 px-5 text-white bg-black rounded-lg"
-                      onClick={createProduct}
-                    >
-                      Нийтлэх
-                    </button>
+                    <button className="border p-3 px-5 text-white bg-black rounded-lg" onClick={createProduct}>Нийтлэх</button>
                   </div>
                 </div>
               </div>
