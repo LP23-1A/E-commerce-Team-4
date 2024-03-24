a82f94cf2f62e33181e7a257c7fa7347aaf88a84
khangerel14 <162080218+khangerel14@users.noreply.github.com>
2024-03-24 18:03:17 +0800

    user navbar footer (#99)
    Co-authored-by: Khangerel Be-ochir <khangeerelbeochir@gmail.com>

---
+++ frontend/src/images/Mail.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import img from "@/../public/mail.svg"
+
+const Mail = () => {
+  return (
+    <div>
+      <img src={img.src} alt="" />
+    </div>
+  )
+}
+
+export default Mail
