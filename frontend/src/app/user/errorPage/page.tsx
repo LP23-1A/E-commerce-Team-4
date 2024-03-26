1eab8b247a6a7a7ef882f3a4d687fd3b24d2507c
Khangerel Be-ochir <23LP5567@G303-CC0142.local>
2024-03-26 09:51:50 +0800

    products ready

--- frontend/src/app/user/errorPage/page.tsx
+++ frontend/src/app/user/errorPage/page.tsx
@@ -11,12 +11,12 @@ const page = () => {
   }
   return (
     <div>
-        <NavbarUser />  
-        <div className='flex flex-col justify-center items-center my-32'>
-            <Error />
-            <button className='bg-[#FB2E86] px-6 rounded-md p-3 text-white' onClick={homePage}>Нүүр хуудас</button>
-        </div>
-        <Footer />
+      <NavbarUser />  
+      <div className='flex flex-col justify-center items-center my-32'>
+        <Error />
+        <button className='bg-[#FB2E86] px-6 rounded-md p-3 text-white' onClick={homePage}>Нүүр хуудас</button>
+      </div>
+      <Footer />
     </div>
   )
 }
