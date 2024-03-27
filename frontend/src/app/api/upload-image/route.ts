9fc94a788d781324bb42cb9cc5ca6f1d307da9d4
батчулуун цогбаяр <23LP2014@G303-CC0138.local>
2024-03-27 14:20:33 +0800

    user order

--- frontend/src/app/api/upload-image/route.ts
+++ frontend/src/app/api/upload-image/route.ts
@@ -1,41 +1,41 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { v4 as uuidv4 } from "uuid";
+// import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+// import { v4 as uuidv4 } from "uuid";
 
-const s3 = new S3Client({
-  region: "ap-southeast-1",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
-  },
-});
+// const s3 = new S3Client({
+//   region: "ap-southeast-1",
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+//   },
+// });
 
-export async function GET(req: Request, res: Response) {
-  try {
-    const objectUrls: any = [];
-    const urls = await Promise.all(
-      ["image1.png"].map((_) => {
-        const key = uuidv4();
-        const command = new PutObjectCommand({
-          Bucket: "ecommerceteam4pinecone",
-          Key: `${key}`,
-          ACL: "public-read",
-        });
+// export async function GET(req: Request, res: Response) {
+//   try {
+//     const objectUrls: any = [];
+//     const urls = await Promise.all(
+//       ["image1.png"].map((_) => {
+//         const key = uuidv4();
+//         const command = new PutObjectCommand({
+//           Bucket: "ecommerceteam4pinecone",
+//           Key: `${key}`,
+//           ACL: "public-read",
+//         });
 
-        objectUrls.push(
-          `https://ecommerceteam4pinecone.s3.ap-southeast-1.amazonaws.com/${key}`
-        );
+//         objectUrls.push(
+//           `https://ecommerceteam4pinecone.s3.ap-southeast-1.amazonaws.com/${key}`
+//         );
 
-        return getSignedUrl(s3, command, { expiresIn: 3600 });
-      })
-    );
+//         return getSignedUrl(s3, command, { expiresIn: 3600 });
+//       })
+//     );
 
-    return Response.json({
-      uploadUrls: urls,
-      objectUrl: objectUrls,
-    });
-  } catch (error) {
-    console.error("Error getting signed URL:", error);
-    throw error;
-  }
-}
+//     return Response.json({
+//       uploadUrls: urls,
+//       objectUrl: objectUrls,
+//     });
+//   } catch (error) {
+//     console.error("Error getting signed URL:", error);
+//     throw error;
+//   }
+// }
