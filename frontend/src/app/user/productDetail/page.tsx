48aeeedbfb3fbd8feca30270116d4ff5e67a503c
Chinguun Sainbat <23LP1573@G303-CC0163.local>
2024-03-27 11:32:25 +0800

    Project detail duslaa

--- frontend/src/app/user/productDetail/page.tsx
+++ frontend/src/app/user/productDetail/page.tsx
@@ -4,7 +4,7 @@ import Rectangle1 from "@/images/Rectangle1";
 import Rectangle2 from "@/images/Rectangle2";
 import Rectangle3 from "@/images/Rectangle3";
 import Rectangle4 from "@/images/Rectangle4";
-import { EmptyStar } from "@/images/emptyStar";
+import { EmptyStar } from "@/images/EmptyStar";
 import { FullStar } from "@/images/FullStar";
 import { OrangeDot } from "@/images/OrangeDot";
 import { PinkDot } from "@/images/PinkDot";
@@ -13,7 +13,6 @@ import Heart from "@/images/Heart";
 import { RightArrow } from "@/images/RightArrow";
 
 import React from "react";
-import { Empty } from "antd";
 
 const page = () => {
   return (
@@ -83,12 +82,57 @@ const page = () => {
           </div>
         </div>
       </div>
-      <div className="w-full h-[456px]">
-        <div className="w-[1440] mx-auto">
-          <h1 className="font-extrabold text-4xl">Холбоотой бүтээгдэхүүн</h1>
-          <div>
-            <div>
-              <div></div>
+      <div className="w-full ">
+        <div className="w-[1440] flex flex-col mx-[auto] gap-[49px] my-[100px]">
+          <h1 className="font-extrabold text-4xl w-[1200px] mx-auto">Холбоотой бүтээгдэхүүн</h1>
+          <div className="flex gap-10 mx-auto">
+            <div className="w-[270px] h-[363px] flex flex-col justify-center items-center gap-[10px]">
+              <div className="h-[280px] bg-[#F6F7FB] p-[20px]">
+                <img src="https://s3-alpha-sig.figma.com/img/5d23/d89f/fb399a001ca2c9596b7e4e167945bbdc?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QtRuQ7scZohUz1uAGd3TPwdB6DAAFv5pvk-SgBJgvIzWpazvDA88mMqq-ONhsJqxbbmDr7sU1TM24DJ7r3gl6kR4FraW3JIAMQxyDvRBkuj8HDejIVsZu3OFLH7V3Maeo9AFVwlHRzysIiDebNq2lP7d5LBRaW9EPaLDt-TNlRdKhCyG1sZdZEgITohSCqH6eOt4RYvDZ9khjTVN4Hss~w70d8aI5RE6a-01Q8mq2JdLBRN9MAH0YGBEjsAwn7ya9frhLmReMTno8wqCpUefGC1SBGy-sp2uEKwVRm~~hruTanezFISpjCPfluxt19LG5JxgeVj9~nf9Sk55dil6Qw__" alt="camera" />
+              </div>
+              <p className="font-bold text-lg/18 text-[#151875] text-center">Canon камер</p>
+              <div className="flex gap-[8px]">
+                <OrangeDot/>
+                <PinkDot/>
+                <BlueDot/>
+              </div>
+              <p className="font-normal text-xl/21 text-[#151875] text-center">750’000₮</p>
+            </div>
+            <div className="w-[270px] h-[363px] flex flex-col justify-center items-center gap-[10px]">
+              <div className="h-[280px] bg-[#EBF4F3] p-[20px]">
+                <img src="https://s3-alpha-sig.figma.com/img/9db5/545f/6002c578779f313538a8a68e1a6f8050?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oznEKJLSEDOSX96A-FKA5WWgpNJDEj6ASTaRzqlXkJGNgC1DTiSMjURwxS3Z6NwNhNJXO9BKSjadnGQDBKtoxD-4ZWIn1Oq7jDIRVDvTqRhgAuf~m3m4aiiBE6hsy5Pim-OOF5EL4DN7iaElZbgBSgrF0Qmkqi-dvYFijggFqUVVUYmbGXeZrB-6FHVU75zQtCFzdUGU~0KXkYsI3VkXjNXXlcUu2lcjKdlp0wP6TJJP3iCnh7kCgNY~RmNqB4PGXkiEz32Llilw57wj6plARviAIlefucgAQ-JNrgWb9M1sJOuPfgB15NuFaJqkjOmKI6EoJPO1QENSTUiejJL82Q__" alt="camera" />
+              </div>
+              <p className="font-bold text-lg/18 text-[#151875] text-center">Усан ягаан сандал</p>
+              <div className="flex gap-[8px]">
+                <OrangeDot/>
+                <PinkDot/>
+                <BlueDot/>
+              </div>
+              <p className="font-normal text-xl/21 text-[#151875] text-center">750’000₮</p>
+            </div>
+            <div className="w-[270px] h-[363px] flex flex-col justify-center items-center gap-[10px]">
+              <div className="h-[280px] bg-[#F6F7FB] p-[20px]">
+                <img src="https://s3-alpha-sig.figma.com/img/795e/f307/40fb6d72006bc9fe7a747b3dfca0a5de?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VRE14vILtUEY2NGoc1314uOIAwt3rweRXT1Xz4afH~3QS45qMoziv9mHb4o3YIu3UI2OvcFAJgCF9wau1eXUgqpjt-c~AiYZqimtxuq~nTp9e~zT-z24f6bLRCp7WzCrkq~EoJoonhyn-dN8lgEb-LzkO7r~glSvblNkd9OW3F9Mvor4OhWJIpT4sEWWEwOXLN4LufhwXXBqVXgPC0CMELRhjxNUt0beH9Eqz6PJKKJvfxLLjn1q-K2oavIUE5h4h9MaMIQtwnAAvbkB-awf1jLJJj3NukG5s2yVnxlay7FFL2va98VKX00mwrnZoemMepzKf3xMm3CZf5jXg5UIrA__" alt="camera" />
+              </div>
+              <p className="font-bold text-lg/18 text-[#151875] text-center">Саарал сандал</p>
+              <div className="flex gap-[8px]">
+                <OrangeDot/>
+                <PinkDot/>
+                <BlueDot/>
+              </div>
+              <p className="font-normal text-xl/21 text-[#151875] text-center">750’000₮</p>
+            </div>
+            <div className="w-[270px] h-[363px] flex flex-col justify-center items-center gap-[10px]">
+              <div className="h-[280px] bg-[#F6F7FB] p-[20px]">
+                <img src="https://s3-alpha-sig.figma.com/img/c298/504d/adeb68467abfedd08c1e1d67526e0d7a?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KxoGlcr~xTJPRW6bRvhe2IvqW018i~JlcUwXEXMfPCZyfksJZe5cyHCZwaAz5jP1nua-VKig8BZA1YDbIK1ua96v~WVjtNuzTZUXQE9G58suDsiixl6e74H69rQ9qr5Kttp9w6qOjrbpi1N-SnoF5wip2bW51X7B-sfsd16kuBXUiMevd~vooEXL5SYHlTwOj2Z~WMxXHG4Mpu4SQF32m3bM9IF8ATpooSiarhUT6zrC-ToR4uwPgczRT4Aw-OPWPesP5YE3L~LwCiBd45sui~iF7DTFlx4Pi~okvR7Ug6dnToIJAj92eva~vHU81PWTgSgwhxl81BXmPOPdpLgx2A__" alt="camera" />
+              </div>
+              <p className="font-bold text-lg/18 text-[#151875] text-center">Laptop цүнх</p>
+              <div className="flex gap-[8px]">
+                <OrangeDot/>
+                <PinkDot/>
+                <BlueDot/>
+              </div>
+              <p className="font-normal text-xl/21 text-[#151875] text-center">750’000₮</p>
             </div>
           </div>
         </div>
