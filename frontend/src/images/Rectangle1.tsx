aadb06b4c0203d034cc12f0bd2948ee54ac0b41f
ChingunS <145735494+ChingunS@users.noreply.github.com>
2024-03-27 12:20:10 +0800

    103 feature product details (#117)
    * product details ehelsen
    
    * Product detail deer ajilj bn
    
    * Project detail duslaa
    
    * project detail duslaa
    
    ---------
    
    Co-authored-by: Chinguun Sainbat <23LP1573@G303-CC0163.local>

---
+++ frontend/src/images/Rectangle1.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import img from "@/../public/Rectangle 134.png"
+
+const Rectangle1 = () => {
+  return (
+    <div>
+        <img src={img.src} alt="" />
+    </div>
+  )
+}
+
+export default Rectangle1
---
+++ frontend/src/images/Rectangle3.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import img from "@/../public/Rectangle 137.png"
+
+const Rectangle3 = () => {
+  return (
+    <div>
+        <img src={img.src} alt="" />
+    </div>
+  )
+}
+
+export default Rectangle3
